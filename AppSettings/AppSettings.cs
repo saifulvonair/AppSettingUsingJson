26bfa940c41f4e57962811e1ffe5a5d0ae7ee15b
Md. Saiful Alam <saiful.alam@bjitgroup.com>
2021-11-04 20:07:25 +0600

    Added seetings code

---
+++ AppSettings/AppSettings.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSettings
+{
+    public class AppSettings
+    {
+        public class SettingsItem
+        {
+            public bool UpdateUIFromSetting;
+            public int ScreenSizeWidth;
+            public int ScreenSizeHeight;
+            public string Title;
+        }
+        #region variables
+
+        private SettingsItem _settingsItem;
+        public SettingsItem Settings { get { return this._settingsItem; } }
+        
+        #endregion
+        
+        public static String filePath = AppDomain.CurrentDomain.BaseDirectory + "Settings.json";
+
+        private static AppSettings appSettings;
+
+        private Delegate _onSettingUpdateEventListener;
+        public Delegate OnSettingUpdateEventListener { get { return _onSettingUpdateEventListener; } set { _onSettingUpdateEventListener = value; } }
+
+        public static AppSettings Instance()
+        {
+            if(appSettings == null)
+            {
+                appSettings = new AppSettings();
+            }
+            return appSettings;
+        }
+
+        private AppSettings()
+        {
+            Load(null);
+        }
+
+        public void Load(Delegate eventHandler)
+        {
+            try
+            {
+                this.OnSettingUpdateEventListener = eventHandler;
+
+                if(_settingsItem == null)
+                {
+                    string jsonString = File.ReadAllText(filePath);
+                    _settingsItem = Newtonsoft.Json.JsonConvert.DeserializeObject<SettingsItem>(jsonString);
+                    if (_settingsItem == null)
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+                NotifySettingsListener();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                LoadDefault();
+                Save();
+            }
+        }
+        public void Save()
+        {
+            try
+            { 
+                string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(_settingsItem);
+                File.WriteAllText(filePath, jsonString);
+                NotifySettingsListener();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void LoadDefault()
+        {
+            this._settingsItem = new SettingsItem();
+            this._settingsItem.ScreenSizeHeight = 500;
+            this._settingsItem.ScreenSizeWidth = 700;
+            this._settingsItem.Title = "Settins Items";
+            NotifySettingsListener();
+        }
+
+        public void NotifySettingsListener()
+        {
+            if (_onSettingUpdateEventListener != null)
+            {
+                _onSettingUpdateEventListener.DynamicInvoke(null);
+            }
+        }
+    }
+}
---
+++ AppSettings/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppSettings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AppSettings.Instance().Load((MethodInvoker)delegate
+            {
+                OnSettingsLoaded();
+            });
+        }
+
+        void OnSettingsLoaded()
+        {
+            if (AppSettings.Instance().Settings.UpdateUIFromSetting)
+            {
+                this.Width = AppSettings.Instance().Settings.ScreenSizeWidth;
+                this.Height = AppSettings.Instance().Settings.ScreenSizeHeight;
+                this.Text = AppSettings.Instance().Settings.Title;
+
+                OnUpdateUISetting();
+            }
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.textBox1.Text = AppSettings.Instance().Settings.Title;
+            this.textBox2.Text = this.Width.ToString();
+            this.textBox3.Text = this.Height.ToString();
+        }
+
+        void OnUpdateUISetting()
+        {
+            this.textBox1.Text = AppSettings.Instance().Settings.Title;
+            this.textBox2.Text = this.Width.ToString();
+            this.textBox3.Text = this.Height.ToString();
+            this.checkBox1.Checked = AppSettings.Instance().Settings.UpdateUIFromSetting;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            OnSaveSettings(null, null);
+        }
+
+        private void OnSaveSettings(object sender, EventArgs e)
+        {
+            AppSettings.Instance().Settings.ScreenSizeWidth = this.Width;
+            AppSettings.Instance().Settings.ScreenSizeHeight = this.Height;
+            AppSettings.Instance().Settings.Title = this.textBox1.Text;
+            AppSettings.Instance().Settings.UpdateUIFromSetting = this.checkBox1.Checked;
+            AppSettings.Instance().Save();
+        }
+    }
+}
